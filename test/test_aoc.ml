dffa51a79db257cc9eccca59be525726540e9bef
Andrei Stepanenko <andrei@ftvkyo.me>
2024-12-02 12:33:37 +0000

    refactor(tests): add a helper function

--- test/test_aoc.ml
+++ test/test_aoc.ml
@@ -1,12 +1,16 @@
 open Alcotest
 
-let test_problem problem filename expected () =
-  let got = Aoc.solve ~problem @@ Arg.read_arg filename in
-  check string "same string" got expected
+let test_problem_io problem filename expected () =
+  let produced = Aoc.solve ~problem @@ Arg.read_arg filename in
+  check string "same string" produced expected
 
-let suite =
-  [ ("d01a", `Quick, test_problem "d01a" "../data/d01.txt" "11")
-  ; ("d01b", `Quick, test_problem "d01b" "../data/d01.txt" "31")
-  ]
+let test_problem problem expected =
+  let id_re = Str.regexp {|^\(d[0-9]+\)[ab]$|} in
+  if Str.string_match id_re problem 0 then
+    let id = Str.matched_group 1 problem in
+    let filename = "../data/" ^ id ^ ".txt" in
+    (problem, `Quick, test_problem_io problem filename expected)
+  else raise @@ Invalid_argument ("could not parse id " ^ problem)
 
+let suite = [ test_problem "d01a" "11"; test_problem "d01b" "31" ]
 let () = Alcotest.run "AoC examples" [ ("Example", suite) ]
