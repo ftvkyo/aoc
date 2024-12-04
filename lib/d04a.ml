377c0ccb4acd53a9b360fdf5be11f6b450f8058c
Andrei Stepanenko <andrei@ftvkyo.me>
2024-12-04 19:34:19 +0000

    fmt(d04a)

--- lib/d04a.ml
+++ lib/d04a.ml
@@ -1,8 +1,11 @@
 type rectangle = char array array
 
-let to_rect lines = Array.map (fun row -> Array.of_seq @@ String.to_seq row) lines
+let to_rect lines =
+  Array.map (fun row -> Array.of_seq @@ String.to_seq row) lines
 
-let from_rect rect = String.concat "\n" @@ Array.to_list @@ Array.map (fun row -> String.of_seq @@ Array.to_seq row) rect
+let from_rect rect =
+  String.concat "\n" @@ Array.to_list
+  @@ Array.map (fun row -> String.of_seq @@ Array.to_seq row) rect
 
 let transpose (rect : rectangle) =
   let rect_x = Array.length rect in
@@ -30,16 +33,17 @@ let rotate_45_ccwise (rect : rectangle) =
   let get x y = try rect.(x + y - (rect_y - 1)).(y) with _ -> '.' in
   Array.init_matrix new_x rect_y get
 
-let transforms : (rectangle -> rectangle) list = [
-  Fun.id;
-  transpose;
-  rotate_45_cwise;
-  rotate_45_ccwise;
-  rotate_180;
-  Fun.compose transpose rotate_180;
-  Fun.compose rotate_45_cwise rotate_180;
-  Fun.compose rotate_45_ccwise rotate_180;
-]
+let transforms : (rectangle -> rectangle) list =
+  [
+    Fun.id;
+    transpose;
+    rotate_45_cwise;
+    rotate_45_ccwise;
+    rotate_180;
+    Fun.compose transpose rotate_180;
+    Fun.compose rotate_45_cwise rotate_180;
+    Fun.compose rotate_45_ccwise rotate_180;
+  ]
 
 let solve (input : string array) : string =
   let rect = to_rect input in
